9d414ba79078b49038242e913a14d9f4950b866d
Victor José Costa Farias <47372055+Petrucchio@users.noreply.github.com>
2022-02-11 11:19:31 -0300

    criação do leitor csv

---
+++ TesteBackendEnContact/Controllers/Models/ContactCsv.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using CsvHelper.Configuration.Attributes;
+using CsvHelper.Configuration;
+
+namespace TesteBackendEnContact.Controllers.Models.ContactCsv
+{
+    public class Csv
+    {
+        [Name("Id")]
+        public int Id { get; set; }
+        [Name("ContactBookId")]
+        public int ContactBookId { get; set; }
+        [Name("CompanyId")]
+        public int CompanyId { get; set; }
+        [Name("Name")]
+        public string Name { get; set; }
+        [Name("Phone")]
+        public string Phone { get; set; }
+        [Name("Email")]
+        public string Email { get; set; }
+        [Name("Address")]
+        public string Address { get; set; }
+    }
+    public class ContactMap : ClassMap<Csv>
+    {
+        public ContactMap()
+        {
+            Map(m => m.Id).Name("Id");
+            Map(m => m.ContactBookId).Name("ContactBookId");
+            Map(m => m.CompanyId).Name("CompanyId");
+            Map(m => m.Name).Name("Name");
+            Map(m => m.Phone).Name("Phone");
+            Map(m => m.Email).Name("Email");
+            Map(m => m.Address).Name("Address");
+        }
+    }
+}
---
+++ TesteBackendEnContact/Controllers/UploadController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CsvHelper.Configuration;
+using System;
+using System.IO;
+using CsvHelper;
+using TesteBackendEnContact.Controllers.Models.ContactCsv;
+
+namespace TesteBackendEnContact.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UploadController : ControllerBase
+    {
+        static void ops(string[] args)
+        {
+            var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+            {
+                HasHeaderRecord = true,
+            };
+
+            using (var reader = new StreamReader("C:/Users/Farias/Documents/GitHub/DesafioBackEndEncontact2021/Contacts_import.csv"))
+            using (var csv = new CsvReader(reader, config))
+            {
+                var contacts = csv.GetRecords<Csv>();
+
+                foreach (var contact in contacts)
+                    Console.WriteLine($"Id: {contact.Id}, ContactBookId: {contact.ContactBookId}, CompanyId: {contact.CompanyId} ");
+            }
+        }
+    }
+}
+
+
+
